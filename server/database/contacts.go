5539a7fdf38d3aee907e3733d8950bafb3e8e1f4
hyperxpizza <wojtek.frackowski@gmail.com>
2021-02-20 23:32:59 +0100

    move contacts to a different file

---
+++ server/database/contacts.go
@@ -0,0 +1,59 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	model "github.com/hyperxpizza/contact-manager/server/graph/model"
+)
+
+//InsertContact created a new contact document
+func InsertContact(contact model.Contact) (string, error) {
+	res, err := database.Collection("contacts").InsertOne(context.Background(), &contact)
+	if err != nil {
+		log.Printf("Error while InsertContract: %v", err)
+		return "", err
+	}
+
+	id := fmt.Sprintf("%v", res.InsertedID)
+
+	return id, nil
+}
+
+//GetContact returns a single contact based on filter
+func GetContact(filter *model.Filter) (*model.Contact, error) {
+	var contact model.Contact
+	err := database.Collection("contacts").FindOne(context.Background(), filter).Decode(&contact)
+	if err != nil {
+		log.Printf("GetContact failed: %v\n", err)
+		return nil, err
+	}
+
+	return &contact, nil
+}
+
+//GetContacts returns multiple contacts based on filter
+func GetContacts(filter *model.Filter) ([]*model.Contact, error) {
+	var contacts []*model.Contact
+
+	cursor, err := database.Collection("contacts").Find(context.Background(), filter)
+	if err != nil {
+		log.Printf("GetContacts failed: %v\n", err)
+		return nil, err
+	}
+
+	for cursor.Next(context.Background()) {
+		var contact model.Contact
+
+		err := cursor.Decode(&contact)
+		if err != nil {
+			log.Printf("Cursor.Decode failed: %v\n", err)
+			return nil, err
+		}
+
+		contacts = append(contacts, &contact)
+	}
+
+	return contacts, nil
+}
--- server/database/database.go
+++ server/database/database.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"time"
 
-	model "github.com/hyperxpizza/contact-manager/server/graph/model"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -50,51 +49,4 @@ func ConnectToDB(username, password, authSource, host string, port int) {
 	cancelFunc = cancel
 
 	fmt.Println("Connected to MongoDB!")
-}
-
-func InsertContact(contact model.Contact) (string, error) {
-	res, err := database.Collection("contacts").InsertOne(context.Background(), &contact)
-	if err != nil {
-		log.Printf("Error while InsertContract: %v", err)
-		return "", err
-	}
-
-	id := fmt.Sprintf("%v", res.InsertedID)
-
-	return id, nil
-}
-
-func GetContact(filter *model.Filter) (*model.Contact, error) {
-	var contact model.Contact
-	err := database.Collection("contacts").FindOne(context.Background(), filter).Decode(&contact)
-	if err != nil {
-		log.Printf("GetContact failed: %v\n", err)
-		return nil, err
-	}
-
-	return &contact, nil
-}
-
-func GetContacts(filter *model.Filter) ([]*model.Contact, error) {
-	var contacts []*model.Contact
-
-	cursor, err := database.Collection("contacts").Find(context.Background(), filter)
-	if err != nil {
-		log.Printf("GetContacts failed: %v\n", err)
-		return nil, err
-	}
-
-	for cursor.Next(context.Background()) {
-		var contact model.Contact
-
-		err := cursor.Decode(&contact)
-		if err != nil {
-			log.Printf("Cursor.Decode failed: %v\n", err)
-			return nil, err
-		}
-
-		contacts = append(contacts, &contact)
-	}
-
-	return contacts, nil
 }
